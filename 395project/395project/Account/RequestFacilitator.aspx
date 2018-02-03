49b34b120cac6301dccc6d93839fdcc2fad08c08
Colin <keysc3@mymacewan.ca>
2018-02-02 17:56:40 -0700

    Add a page to request facilitators

---
+++ 395project/395project/Account/RequestFacilitator.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="Request Facilitator Form" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="RequestFacilitator.aspx.cs" Inherits="_395project.Account.RequestFacilitator" %>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <h2><%: Title %></h2>
+    <p class="error-text">
+        <asp:Literal runat="server" ID="ErrorMessage" />
+    </p>
+    <div class="form-horizontal">
+        <div class="form-group">
+			<asp:Label runat="server" AssociatedControlID="FacilitatorFirst" CssClass="col-md-4 input-header">Facilitator First Name</asp:Label>
+			<div class="col-md-10">
+                <asp:TextBox runat="server" ID="FacilitatorFirst" CssClass="inputfields" />
+			</div>
+		</div>
+		<div class="form-group">
+			<asp:Label runat="server" AssociatedControlID="FacilitatorLast" CssClass="col-md-4 input-header">Facilitator Last Name</asp:Label>
+			<div class="col-md-10">
+                <asp:TextBox runat="server" ID="FacilitatorLast" CssClass="inputfields" />
+			</div>
+		</div>
+		<div class="form-group">
+            <div class="col-md-offset-2 col-md-10">
+                <asp:Button runat="server" Text="Request Facilitator" CssClass="mybutton" />
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ 395project/395project/Account/RequestFacilitator.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using _395project.Models;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace _395project.Account
+{
+    public partial class RequestFacilitator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void AddFacilitator_Click(object sender, EventArgs e)
+        {
+            /*
+            //Check if account already exists before creating it
+            var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var check = manager.FindByName(FacilitatorEmail.Text);
+            if (check == null)
+            {
+                ErrorMessage.Text = "There is no account with that email";
+                FacilitatorEmail.Text = string.Empty;
+            }
+            else
+            {
+                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+                conn.Open();
+                string insert = "insert into Facilitators(Id,FirstName, LastName) values (@Email,@FacilitatorFirst, @FacilitatorLast)";
+                SqlCommand cmd = new SqlCommand(insert, conn);
+                cmd.Parameters.AddWithValue("@Email", FacilitatorEmail.Text);
+                cmd.Parameters.AddWithValue("@FacilitatorFirst", FacilitatorFirst.Text);
+                cmd.Parameters.AddWithValue("@FacilitatorLast", FacilitatorLast.Text);
+                cmd.ExecuteNonQuery();
+
+                //Remove if one of the fields is empty
+                string remove = "delete from Facilitators where ID = '' or FirstName = '' or LastName = ''";
+                SqlCommand rm = new SqlCommand(remove, conn);
+                rm.ExecuteNonQuery();
+                conn.Close();
+                FacilitatorFirst.Text = string.Empty;
+                FacilitatorLast.Text = string.Empty;
+                ErrorMessage.Text = "Facilitator Successfully Added";
+            }
+            */
+        }
+    }
+}
