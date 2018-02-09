ef3be000b05c34d7f7adc50e854ab059dcd28b7a
David Dowie <dowie@Davids-MacBook-Pro.local>
2018-02-09 14:16:56 -0700

    Fixes signup pages`

--- 395project/395project/dash/SignUp.aspx.cs
+++ 395project/395project/dash/SignUp.aspx.cs
@@ -7,19 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace _395project.dash
 {
-    public partial class SignUp : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            base.OnPreInit(e);
-            if (User.IsInRole("SuperUser"))
-                MasterPageFile = "/Master/Main.master";
-            else if (User.IsInRole("Admin"))
-                MasterPageFile = "/Master/BoardMember.master";
-            else if (User.IsInRole("Teacher"))
-                MasterPageFile = "/Master/Teacher.master";
-            else if (User.IsInRole("Facilitator"))
-                MasterPageFile = "/Master/Facilitator.master";
-        }
-    }
+	public partial class Signup : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
 }
