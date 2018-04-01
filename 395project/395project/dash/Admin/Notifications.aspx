bd47362c6b640d3c2fce93b01e9909228bf58905
Colin <colin.j.keys@gmail.com>
2018-04-01 17:50:21 -0600

    Confirm popups for notifications

--- 395project/395project/dash/Admin/Notifications.aspx
+++ 395project/395project/dash/Admin/Notifications.aspx
@@ -18,6 +18,62 @@
         <asp:PlaceHolder id="alerts" runat="server" />
     </div>
 
+    <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+
+    <ajaxToolkit:ModalPopupExtender ID="ConfirmPopupExtender" runat="server" PopupControlID="facilitatorConfirm" 
+        BackgroundCssClass="modalBackground" TargetControlID="btn" X="-1" Y="-1" 
+		RepositionMode="RepositionOnWindowResizeAndScroll"></ajaxToolkit:ModalPopupExtender>
+
+    <asp:Panel ID="facilitatorConfirm"  CssClass="dashboardMargins" runat="server">
+        <div class="dashboardMargins" id="notiAdd">
+            <asp:Label runat="server" CssClass="section-header">Facilitator Addition</asp:Label>
+        </div>
+        <div class="row" style="padding-bottom:10px">
+            <div class="col-md-12">
+	            <div class="dashboardMargins" id="notiConfirmTxt">
+		            <asp:Label ID="infoLabel" runat="server" CssClass="input-header"></asp:Label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="dashboardMargins" id="notiFacilButtons" style="padding-left: 100px;">
+			        <asp:Button ID="facilConfirmButton" runat="server" Text="Confirm" OnClick="Confirm_Click" CssClass="mybutton"/>
+			        <asp:Button ID="facilCancelButton" runat="server" Text="Cancel" OnClick="cancelFacilitator" CssClass="mybutton"/>
+                </div>
+            </div>
+        </div>
+     </asp:Panel>
+
+    <ajaxToolkit:ModalPopupExtender ID="AbsencePopupExtender" runat="server" PopupControlID="absenceConfirmPanel" 
+        BackgroundCssClass="modalBackground" TargetControlID="btn" X="-1" Y="-1" 
+		RepositionMode="RepositionOnWindowResizeAndScroll"></ajaxToolkit:ModalPopupExtender>
+
+    <asp:Panel ID="absenceConfirmPanel"  CssClass="dashboardMargins" runat="server">
+        <div class="dashboardMargins" id="absenceAdd">
+            <asp:Label runat="server" CssClass="section-header">Grant Absence</asp:Label>
+        </div>
+        <div class="row" style="padding-bottom:10px">
+            <div class="col-md-12">
+	            <div class="dashboardMargins" id="absenceConfirmTxt">
+		            <asp:Label ID="infoLabel2" runat="server" CssClass="input-header"></asp:Label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="dashboardMargins" id="notiAbsButtons" style="padding-left: 100px;">
+			        <asp:Button ID="confirmAbsence" runat="server" Text="Confirm" OnClick="Confirm_Click" CssClass="mybutton"/>
+			        <asp:Button ID="cancelAbsence" runat="server" Text="Cancel" OnClick="cancelAbsenceReq" CssClass="mybutton"/>
+                </div>
+            </div>
+        </div>
+     </asp:Panel>
+	<!-- Hidden button to link to modalpopup -->
+	<div style="visibility:hidden">
+		<asp:Button id="btn" runat="server"/>
+	</div>
+
 </asp:Content>
 <asp:Content ID="Content5" ContentPlaceHolderID="footer" runat="server">
 </asp:Content>
--- 395project/395project/dash/Admin/Notifications.aspx.designer.cs
+++ 395project/395project/dash/Admin/Notifications.aspx.designer.cs
@@ -38,5 +38,113 @@ namespace _395project.dash.Admin {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.PlaceHolder alerts;
+        
+        /// <summary>
+        /// ScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+        
+        /// <summary>
+        /// ConfirmPopupExtender control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.ModalPopupExtender ConfirmPopupExtender;
+        
+        /// <summary>
+        /// facilitatorConfirm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel facilitatorConfirm;
+        
+        /// <summary>
+        /// infoLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label infoLabel;
+        
+        /// <summary>
+        /// facilConfirmButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button facilConfirmButton;
+        
+        /// <summary>
+        /// facilCancelButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button facilCancelButton;
+        
+        /// <summary>
+        /// AbsencePopupExtender control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.ModalPopupExtender AbsencePopupExtender;
+        
+        /// <summary>
+        /// absenceConfirmPanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel absenceConfirmPanel;
+        
+        /// <summary>
+        /// infoLabel2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label infoLabel2;
+        
+        /// <summary>
+        /// confirmAbsence control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button confirmAbsence;
+        
+        /// <summary>
+        /// cancelAbsence control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button cancelAbsence;
+        
+        /// <summary>
+        /// btn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn;
     }
 }
