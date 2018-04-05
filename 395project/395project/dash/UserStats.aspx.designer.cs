7dc4c51abe5f451b09121fe5ad748e12977b842b
Colin <colin.j.keys@gmail.com>
2018-04-04 19:38:57 -0600

    Stat pages alignment

--- 395project/395project/dash/UserStats.aspx.designer.cs
+++ 395project/395project/dash/UserStats.aspx.designer.cs
@@ -130,6 +130,15 @@ namespace _395project.dash {
         protected global::System.Web.UI.WebControls.Label yearlyHoursLabel;
         
         /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
         /// TotalStatsGridView control.
         /// </summary>
         /// <remarks>
